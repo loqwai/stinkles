1e37329d261169534bd11d667bf9410f2bab7965
Roy van de Water <roy.v.water@gmail.com>
2025-01-10 16:57:12 -0700

    Add arg parsing to cli

--- index.ts
+++ index.ts
@@ -1,6 +1,20 @@
+#! /usr/bin/env bun
 import { generateWorld } from "./src/world";
 
 const main = async () => {
+  let input = "";
+  process.stdin.on("data", (chunk) => {
+    input += chunk;
+  });
+
+  let stdinData = "";
+  for await (const chunk of process.stdin) {
+    stdinData += chunk;
+  }
+
+  const parsedInput = JSON.parse(stdinData);
+  console.log(JSON.stringify(parsedInput, null, 2));
+
   const world = await generateWorld({ seed: 1 });
   console.log(JSON.stringify(world, null, 2));
 };
