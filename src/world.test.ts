66fc2e98a3220cf0c9cfcb02d94ca47ac148fefa
Roy van de Water <roy.v.water@gmail.com>
2025-01-10 16:57:12 -0700

    Let user ask for what is in the room

--- src/world.test.ts
+++ src/world.test.ts
@@ -1,22 +1,22 @@
-import { generateWorld } from "./world";
+import { generateWorld, interact } from "./world";
 import { describe, it, beforeEach, expect } from "bun:test";
 
 describe("world", () => {
   describe("generateWorld", () => {
-    let world: string;
+    let world: Awaited<ReturnType<typeof generateWorld>>;
 
     beforeEach(async () => {
       world = await generateWorld({ seed: 0 });
     });
 
     it("should generate a world", async () => {
-      expect(world).toBeDefined();
+      expect(world.reply).toBeDefined();
     });
   });
 
   describe("generateWorld with seed when called twice", () => {
-    let world1: string;
-    let world2: string;
+    let world1: Awaited<ReturnType<typeof generateWorld>>;
+    let world2: Awaited<ReturnType<typeof generateWorld>>;
 
     beforeEach(async () => {
       world1 = await generateWorld({ seed: 1 });
@@ -28,5 +28,50 @@ describe("world", () => {
     });
   });
 
-  describe("when the user asks what is in a room", () => {});
+  describe("Given a world", () => {
+    let state: Awaited<ReturnType<typeof interact>>;
+
+    beforeEach(async () => {
+      state = await generateWorld({ seed: 1 });
+    });
+
+    describe("Given a world, when the user asks what is in a room", () => {
+      beforeEach(async () => {
+        state = await interact({
+          ...state,
+          question: "What is in the room?",
+        });
+      });
+
+      it("should reply that there is a chest in the room", () => {
+        expect(state.reply).toContain("chest");
+      });
+
+      describe("When the user opens the chest", () => {
+        beforeEach(async () => {
+          state = await interact({
+            ...state,
+            question: "Open the chest",
+          });
+        });
+
+        it("should reply that there is a key in the chest", () => {
+          expect(state.reply).toContain("key");
+        });
+
+        // describe("When the user takes the key", () => {
+        //   beforeEach(async () => {
+        //     state = await interact({
+        //       ...state,
+        //       question: "Take the key",
+        //     });
+        //   });
+
+        //   it("should call the addItemToInventory tool", () => {
+        //     expect(state.inventory).toContain("key");
+        //   });
+        // });
+      });
+    });
+  });
 });
--- src/world.ts
+++ src/world.ts
@@ -1,35 +1,65 @@
 import { strict as assert } from "assert";
 
 export const generateWorld = async ({ seed }: { seed: number }) => {
+  const messages = [
+    {
+      role: "system",
+      content: `
+        You are a game master for a role playing game. You are having a conversation with a player. You
+        are asked to generate one sentence responses from the perspective of a player in the game.
+      `,
+    },
+    {
+      role: "user",
+      content: `Generate a one sentence description of the room I am in`,
+    },
+  ];
+
+  return await interact({
+    question: "Generate a one sentence description of the room I am in",
+    messages,
+    seed,
+  });
+};
+
+export const interact = async ({
+  question,
+  messages,
+  seed,
+}: {
+  question: string;
+  messages: { role: string; content: string }[];
+  seed: number;
+}) => {
+  messages.push({ role: "user", content: question });
+
   const response = await fetch("http://localhost:11434/api/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       model: "llama3.2",
-      messages: [
-        {
-          role: "system",
-          content: `
-            You are a game master for a role playing game. You are having a conversation with a player. You
-            are asked to generate one sentence responses from the perspective of a player in the game.
-          `,
-        },
-        {
-          role: "user",
-          content: `Generate a one sentence description of the room I am in`,
-        },
-      ],
+      messages,
       stream: false,
       options: {
         seed,
       },
+      // tools: [
+      //   {
+      //     name: "addItemToInventory",
+      //     description: "Add an item to the player's inventory",
+      //     parameters: {
+      //       type: "object",
+      //       properties: { item: { type: "string" } },
+      //     },
+      //   },
+      // ],
     }),
   });
 
-  assert(response.ok, "Failed to generate world: " + response.statusText);
+  assert(response.ok, "Failed to interact: " + response.statusText);
 
   const json = await response.json();
-  const content = json.message.content;
-  console.log(content);
-  return content;
+  messages.push(json.message);
+  const reply = json.message.content;
+  return { reply, messages, seed, inventory: [] };
 };
