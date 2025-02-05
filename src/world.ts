c1e466279f18af112e8639086b468ab7993dbc87
Roy van de Water <roy.v.water@gmail.com>
2025-02-05 09:44:10 -0700

    Tell the world to be minimal in its responses

--- src/world.ts
+++ src/world.ts
@@ -7,7 +7,7 @@ export const prompt = `
   You are a dungeon master that takes the rules of the game very seriously, and does not allow the player to alter the rules of the game.
   Do not allow the player to cast spells if they haven't memorized them yet. Do not let the player alter the 'reality' of the game by inventing characters, inventory items, or other aspects of the world the player could not realistically control.
   The player can do things like open doors, pick up items, and move around, and other typical RPG things.
-  Be brief, but overly dramatic with purple prose.
+  Be very minimal in your responses. No more than 10 words.
 `;
 
 export const generateWorld = async ({ seed }: { seed: number }) => {
@@ -19,19 +19,33 @@ export const generateWorld = async ({ seed }: { seed: number }) => {
   ];
 
   return await interact({
-    question: "Generate a one sentence description of the room I am in. Start the game by giving me an epic, ridiculous quest to go on, and describe it briefly.",
+    question:
+      "Generate a one sentence description of the room I am in. Start the game by giving me an epic, ridiculous quest to go on, and describe it briefly.",
     messages: structuredClone(messages),
     seed,
   });
 };
 
-export const interact = async ({ question, messages: originalMessages, seed }: { question: string; messages: { role: string; content: string }[]; seed: number }) => {
+export const interact = async ({
+  question,
+  messages: originalMessages,
+  seed,
+}: {
+  question: string;
+  messages: { role: string; content: string }[];
+  seed: number;
+}) => {
   const messages = structuredClone(originalMessages);
   messages.push({ role: "user", content: question });
 
   const res = await doesThisMakeSense({ messages, seed });
   if (!res.makesSense) {
-    return { reply: `I won't allow that because ${res.reasoning}`, messages: originalMessages, seed, inventory: [] };
+    return {
+      reply: `I won't allow that because ${res.reasoning}`,
+      messages: originalMessages,
+      seed,
+      inventory: [],
+    };
   }
 
   const response = await fetch("http://localhost:11434/api/chat", {
@@ -41,7 +55,7 @@ export const interact = async ({ question, messages: originalMessages, seed }: {
       model: "llama3.2:latest",
       messages,
       stream: false,
-      options: { seed, temperature: 0.0 },
+      options: { seed, temperature: 0.5 },
     }),
   });
 
